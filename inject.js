e0eea56a8abf81f1713566d99e3a76bf12861897
Jens Langhammer <jens.langhammer@beryju.org>
2018-05-08 21:14:40 +0200

    only inject css when vCenter H5 client is detected
    closes #1

--- inject.js
+++ inject.js
@@ -1,7 +1,13 @@
-['css/clr-ui-dark.css', 'css/fixes.css'].forEach(function (styleSheet) {
-    var style = document.createElement('link');
-    style.rel = 'stylesheet';
-    style.type = 'text/css';
-    style.href = chrome.extension.getURL(styleSheet);
-    (document.head || document.documentElement).appendChild(style);
-});
+// Dark-vCenter JS inject
+
+// Check if we're actually in vCenter
+// this checks that the 'vSphere Client' span exists
+if (document.querySelector("[aria-label='Product home']") !== null) {
+    ['css/clr-ui-dark.css', 'css/fixes.css'].forEach(function (styleSheet) {
+        var style = document.createElement('link');
+        style.rel = 'stylesheet';
+        style.type = 'text/css';
+        style.href = chrome.extension.getURL(styleSheet);
+        (document.head || document.documentElement).appendChild(style);
+    });
+}
