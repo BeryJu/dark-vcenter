273626177d4a2af14c9901ade2c9222b4620661c
Jens Langhammer <jens.langhammer@beryju.org>
2018-05-08 21:28:59 +0200

    separate css into smaller files, add new OS icons from wikimedia
    closes #9

--- inject.js
+++ inject.js
@@ -3,7 +3,16 @@
 // Check if we're actually in vCenter
 // this checks that the 'vSphere Client' span exists
 if (document.querySelector("[aria-label='Product home']") !== null) {
-    ['css/clr-ui-dark.css', 'css/fixes.css'].forEach(function (styleSheet) {
+    var cssFiles = [
+        'css/clr-ui-dark.css',
+        'css/fixes.css',
+        'css/datagrid-fix.css',
+        'css/icon-fix.css',
+        'css/scrollbar-fix.css',
+        'css/stack-fix.css',
+        'css/treeview-fix.css',
+    ];
+    cssFiles.forEach(function (styleSheet) {
         var style = document.createElement('link');
         style.rel = 'stylesheet';
         style.type = 'text/css';
