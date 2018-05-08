fc9a06879a1bbcb993cafc7a09da72fc05d1e0cd
Jens Langhammer <jens.langhammer@beryju.org>
2018-05-08 21:48:01 +0200

    add konami code, enables comic sans
    closes #10
    closes #11

--- inject.js
+++ inject.js
@@ -1,5 +1,13 @@
 // Dark-vCenter JS inject
 
+var injectCss = function (file) {
+    var style = document.createElement('link');
+    style.rel = 'stylesheet';
+    style.type = 'text/css';
+    style.href = chrome.extension.getURL(file);
+    (document.head || document.documentElement).appendChild(style);
+}
+
 // Check if we're actually in vCenter
 // this checks that the 'vSphere Client' span exists
 if (document.querySelector("[aria-label='Product home']") !== null) {
@@ -12,11 +20,22 @@ if (document.querySelector("[aria-label='Product home']") !== null) {
         'css/stack-fix.css',
         'css/treeview-fix.css',
     ];
-    cssFiles.forEach(function (styleSheet) {
-        var style = document.createElement('link');
-        style.rel = 'stylesheet';
-        style.type = 'text/css';
-        style.href = chrome.extension.getURL(styleSheet);
-        (document.head || document.documentElement).appendChild(style);
+    cssFiles.forEach(function (file) {
+        injectCss(file);
     });
 }
+
+// Konami Code mechanism
+var konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+var konamiCodePosition = 0;
+document.addEventListener('keydown', function (e) {
+    if (e.keyCode == konamiCode[konamiCodePosition]) {
+        konamiCodePosition++;
+        if (konamiCodePosition == konamiCode.length) {
+            injectCss('css/konami-fix.css');
+            konamiCodePosition = 0;
+        }
+    } else {
+        konamiCodePosition = 0;
+    }
+});
